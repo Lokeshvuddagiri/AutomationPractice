43832031d87be2e7f326b36759c1b3b45ee5a055
Lokesh Vuddagiri <lokesh.vuddagiri1@gmail.com>
2023-11-04 17:15:50 +0530

    Locators code

--- automation/src/main/java/seleniumpreparation/LocatorsPractice.java
+++ automation/src/main/java/seleniumpreparation/LocatorsPractice.java
@@ -1,5 +1,8 @@
 package seleniumpreparation;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,12 +13,15 @@ public class LocatorsPractice {
 		// TODO Auto-generated method stub
     WebDriver driver= new ChromeDriver();
     driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     driver.get("https://rahulshettyacademy.com/locatorspractice/");
     driver.findElement(By.id("inputUsername")).sendKeys("Lokesh");
     driver.findElement(By.name("inputPassword")).sendKeys("Password");
     driver.findElement(By.name("chkboxTwo")).click();
     driver.findElement(By.className("signInBtn")).click();
+    System.out.println(   driver.findElement(By.cssSelector("p.error")).getText());
     
+ 
     
 	}
 
