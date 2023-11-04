c42909dbaa9f84360da8d3962541d206f103376c
Lokesh Vuddagiri <lokesh.vuddagiri1@gmail.com>
2023-11-04 16:29:46 +0530

    Locators Practice

---
+++ automation/src/main/java/seleniumpreparation/LocatorsPractice.java
@@ -0,0 +1,22 @@
+package seleniumpreparation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LocatorsPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    WebDriver driver= new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://rahulshettyacademy.com/locatorspractice/");
+    driver.findElement(By.id("inputUsername")).sendKeys("Lokesh");
+    driver.findElement(By.name("inputPassword")).sendKeys("Password");
+    driver.findElement(By.name("chkboxTwo")).click();
+    driver.findElement(By.className("signInBtn")).click();
+    
+    
+	}
+
+}
