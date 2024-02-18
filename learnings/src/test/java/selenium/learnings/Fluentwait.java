339de8bb134e410d2c0e4ace9886dd4e59c06550
Lokesh Vuddagiri <lokesh.vuddagiri1@gmail.com>
2024-02-18 14:51:17 +0530

    These are the latest changes

---
+++ learnings/src/test/java/selenium/learnings/Fluentwait.java
@@ -0,0 +1,13 @@
+package selenium.learnings;
+
+public class Fluentwait {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+
+	}
+
+}
